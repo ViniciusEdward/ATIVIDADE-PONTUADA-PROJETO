9c5ed0c02b86afcfe649993d8b0a9c0969f1e3d8
Vinicius <viniciusedward0307@gmail.com>
2024-10-29 17:33:23 +0000

    salvando

--- vini.py
+++ vini.py
@@ -1,5 +1,7 @@
-import os
-os.system("cls || clear")
+def limpar_terminal():
+    import os
+    os.system("cls || clear")
+
 def biblioteca():
     print("""=== Bem vindo à nossa biblioteca ===
     1 - Criar cadastro para comprar um livro
@@ -9,7 +11,7 @@ def biblioteca():
     5 - Estoque de livros
     6 - Carrinho de compras
     0 - Sair do sistema
-    (Para comprar/alugar um livro, primeiro solicite ver o estoque e depois coloque o id do livro que deseja)
+    (Para comprar/alugar um livro, primeiro faça seu cadastro, depois solicite ver o estoque e coloque o id do livro que deseja)
     """)
 
 def cadastro():
@@ -25,7 +27,7 @@ def calcular_preco(preco, forma_de_pagamento):
         return preco - desconto
     return preco + desconto
 
-def compras_de_livros(carrinho):
+def compras_de_livros(carrinho, carrinho_de_livros):
     livros = {
         '123': ('Anabele', 150.00),
         '456': ('Chucky', 170.00),
@@ -43,16 +45,34 @@ def compras_de_livros(carrinho):
         nome, preco = livros[codigo]
         print(f"Livro escolhido: {nome}")
         print(f"Valor do livro: R$ {preco:.2f}")
-        forma_de_pagamento = input("Digite a forma de pagamento(À vista (A) e Crédito (C)): ")
+        forma_de_pagamento = input("Digite a forma de pagamento(À vista (A) e Crédito (C)): ").upper()
         total_a_pagar = calcular_preco(preco, forma_de_pagamento)
         carrinho.append(total_a_pagar)
+        carrinho_de_livros.append(nome)
         print(f"Total à pagar: R$ {total_a_pagar:.2f}")
     else:
         print("Código do livro inválido!")
 
 
-def alugar_um_livro():
-    pass
+def alugar_um_livro(carrinho_de_livros):
+    livros = {
+    '123': ('Anabele'),
+    '456': ('Chucky'),
+    '789': ('It - A coisa'),
+    '111': ('Diário de um banana'),
+    '112': ('Auto da compadecida'),
+    '113': ('A troca'),
+    '101': ('Romeu e Julieta'),
+    '102': ('Orgulho e preconceito'),
+    '103': ('A culpa é das estrelas'),
+}
+
+    codigo = input("Digite o código do livro que deseja: ")
+    if codigo in livros:
+        nome = livros[codigo]
+        print(f"\nLivro à ser alugado: {nome}")
+        print("Você tem um mês de aluguel. Após esse um mês, você vai pagar uma taxa de R$5,00 por cada semana de atraso")
+        carrinho_de_livros.append(nome)
 
 def devolucao_de_um_livro():
     pass
@@ -62,19 +82,24 @@ def estoque_de_livro():
     Estoque de livros:
     Livros de terror: 
     Anabele (123), Chucky (456), It - A coisa (789)
+          
     Livros de humor: 
     Diário de um banana (111), Auto da compadecida (112), A troca (113)
+          
     Livros de romance: 
     Romeu e Julieta (101), Orgulho e preconceito (102), A culpa é das estrelas (103)
     """)
 
-def exibir_carrinho(carrinho):
+def exibir_carrinho(carrinho, carrinho_de_livros):
     soma = sum(carrinho)
+    for livro in carrinho_de_livros():
+        print(f"Livros escolhidos: {livro}")
     print(f"Total à pagar: {soma:.2f}")
 
-os.system("cls || clear")
+limpar_terminal()
 biblioteca()
 carrinho = []
+carrinho_de_livros = []
 while True:
     opcao = input("\nDigite o que deseja: ")
     match(opcao):
@@ -82,22 +107,21 @@ while True:
             cadastro()
 
         case '2':
-            compras_de_livros(carrinho)
-            pass
+            compras_de_livros(carrinho, carrinho_de_livros)
 
         case '3':
-            alugar_um_livro()
-            pass
+            alugar_um_livro(carrinho_de_livros)
+            
 
         case '4':
             devolucao_de_um_livro()
-            pass
-
+            
         case '5':
             estoque_de_livro()
-            pass
+            
         case '6':
-            exibir_carrinho(carrinho)
+            exibir_carrinho(carrinho, carrinho_de_livros)
+            break
 
         case _:
             print("Saindo do sistema")
